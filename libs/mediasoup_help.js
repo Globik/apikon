695bc17bb512dad0d1477957d97fc19a1444ea5e
Globik <globalikslivov@gmail.com>
2024-06-28 04:22:50 +0500

    a

--- libs/mediasoup_help.js
+++ libs/mediasoup_help.js
@@ -554,8 +554,8 @@ const mediasoupOptions = {
     listenInfos:[
     {
 		protocol:"udp",
-		ip:(process.env.DEVELOPMENT == "yes" ? '127.0.0.1' : "45.12.18.172"),
-		announcedAddress: null
+		ip:(process.env.DEVELOPMENT == "yes" ? '127.0.0.1' : "0.0.0.0"),
+		announcedAddress: "45.12.18.172"
 	}],
     enableUdp: true,
     enableTcp: false,
