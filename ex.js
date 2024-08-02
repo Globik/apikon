b525b9efe8c9fd7adae432c13fe422a3eb06437b
Globik <globalikslivov@gmail.com>
2024-08-02 21:19:42 +0500

    a

---
+++ ex.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const axios=require('axios').default;
+const app = express()
+app.use(express.static('./test'));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
