52c51a8be3c86ab9f7cc7fd60f06cbfe5fb64266
Globik <globalikslivov@gmail.com>
2024-09-07 12:13:45 +0500

    a

--- public/js/somevkcb.js
+++ public/js/somevkcb.js
@@ -1,3 +1,3 @@
  console.log('href ', window.location.href);
  const paramStr = new URLSearchParams(window.location.href);
- out.textContent = 'code ' + partamStr.get('code') + ' device_id ' + paramStr.get('device_id');
+ out.textContent = 'code ' + paramStr.get('code') + ' device_id ' + paramStr.get('device_id');
