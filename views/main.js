42b4494193b0b36dc87c2f4e8b10c20e9c0cd26e
Globik <globalikslivov@gmail.com>
2024-09-19 15:51:33 +0500

    a

--- views/main.js
+++ views/main.js
@@ -167,6 +167,7 @@ console.log('not initialised');
     <input type="hidden" id="isEnter" value="${n.user?n.user.entr==0?true:false:false}" />
     <input type="hidden" id="VKID" value="${n.buser?n.buser.vkid:null}" />
     <!-- ${n.buser? JSON.stringify(n.buser):'no buser'} -->
+    <!-- ${n.user? JSON.stringify(n.uuser):'no user'} -->
     
     ${!n.user?`<script>
     const NICK = "anon";
