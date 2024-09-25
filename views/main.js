6ed5cbd71cd361d8b854164211ecde30163ba9fb
Globik <globalikslivov@gmail.com>
2024-09-25 22:08:16 +0500

    a

--- views/main.js
+++ views/main.js
@@ -295,7 +295,15 @@ window.onhashchange = function(ev){
 
   
      </script>
-     
+     <div id="yandex_rtb_R-A-12098170-2"></div>
+     <script>
+     window.yaContextCb.push(()=>{
+		 Ya.Context.AdvManager.render({
+			 "blockId":"R-A-12098170-2",
+			 "renderTo":"yandex_rtb_R-A-12098170-2"
+		 })
+	 })
+	 </script>
      <article id="mediabox">
     <nav id="navpanel"><div class="nav"><b>Online: <span id="onlineCount">0</span></b>&nbsp;&nbsp;&nbsp; ${n.user?n.user.name:'anonym'}${n.VK?'':`&nbsp;&nbsp;&nbsp;<b style="font-size:18px;">&#x1F441;</b>&nbsp;&nbsp;&nbsp;
     <span id="vV" style="color:orange;font-weight:bold;">${n.imgData && n.imgData.img_data?n.imgData.value:0}</span>`}
