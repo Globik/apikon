82bfccf026020cbff144432a1cff82d2163fab05
Globik <globalikslivov@gmail.com>
2024-10-20 13:35:08 +0500

    a

--- views/main.js
+++ views/main.js
@@ -515,7 +515,8 @@ ${n.VK?'':`<div id="giftbox">
 lang=='en'?'About us':
 lang=='zh'?'关于我们':
 lang=='id'?'tentang kami':''}</a> -->
-<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-share-url="https://core.telegram.org/widgets/share" data-text="notext"></script>
+<a href='https://t.me/share/url?url=${encodeURI("https://rouletka.ru/about")}&text=${encodeURI('Чат рулетка')}'>share</a>
+<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-share-url="https://core.telegram.org/widgets/share"></script>
 <script>document.write(VK.Share.button());</script>
 </div>
 </div>
