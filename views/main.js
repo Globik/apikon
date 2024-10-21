d979a6dd8995cf6ab085986798cf50bca3f6ff7c
Globik <globalikslivov@gmail.com>
2024-10-21 15:35:29 +0500

    a

--- views/main.js
+++ views/main.js
@@ -521,9 +521,9 @@ lang=='id'?'tentang kami':''}</a> -->
 <img style="display:inline;transform:translateY(2px);padding-left:5px;" src="/img/telega.png" width="22px" height="20px"/></a>
 <b style="display:inline-block;margin:3px;line-height:1.3;padding-bottom:1px;transform:translateY(-2px);color:blue;font-size:0.9rem;">Share</b></div>
 <div style="margin-left:4px;"><script>document.write(VK.Share.button(false,{type:"round",text:"Share"}));</script></div>
---></div>
+-->
 <div class="ya-share2" data-curtain data-size="m" data-shape="round" data-services="vkontakte,telegram" data-url="https://rouletka.ru/about"  data-image="https://rouletka.ru/og_image.png"></div>
-</div>
+</div></div>
 ${n.VK?`<div id="chatruleslink"><a href="#regeln">Правила чата</a></div>`:''}
 <script>
 if(isVK.value=="true"){
