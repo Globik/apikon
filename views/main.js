fb6bb17d6203b9419102c03754ab7dfb73949a4b
Globik <globalikslivov@gmail.com>
2024-11-12 19:08:34 +0500

    a

--- views/main.js
+++ views/main.js
@@ -719,7 +719,7 @@ if(isLogin.value=="false") getReklama();
     <output id="bannedoutput" class="popi">
     ${lang=='ru'?`
     <p>Вы забанены за нарушение правил чата!<!-- ${n.user?n.user.grund=="1"?'темный экран, за закрытый экран, за демонстрацию мебели':n.user.grund=="2"?'онанизм':'':''} --></p>
-    <p>Чтобы разбанить себя, оплатите 30 рублей.</p>
+    <p>Чтобы разбанить себя, оплатите 300 рублей.</p>
     <form id="bannedForm" method="post" action="https://yoomoney.ru/quickpay/confirm" name="ordertodo">
     <div><input type="hidden"  placeholder="Получатель yoomoney" name="receiver" value="4100118676103827" required/> </div>
 	<input type="hidden" name="label" value="id=${n.user?n.user.id:'0'}&p=300?ip=${n.user?n.user.ip?n.user.ip:undefined:undefined}"/>
@@ -727,7 +727,7 @@ if(isLogin.value=="false") getReklama();
     <input type="hidden" name="successURL" value="https://rouletka.ru/about" />
     <input type="hidden" name="formcomment" value="Разбан" />
     <input type="hidden" name="targets" value="Разбан" />
-    <div><input class="number"  type="hidden" name="sum" value="30.00" required data-type="number"/></div>
+    <div><input class="number"  type="hidden" name="sum" value="300.00" required data-type="number"/></div>
    <input  class="input" type="hidden" checked name="paymentType" value="PC" /></div>
    <div><input  class="input" type="hidden" name="paymentType" value="AC" /></div>
    <div class="bandiv"><input type="submit"  value="Разбанить"/></div>
