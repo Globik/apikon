33ff91d19c1bf7420fddd46dcf55131c0bd3f8ab
Globik <globalikslivov@gmail.com>
2025-05-22 09:26:28 +0500

    a

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -1853,7 +1853,7 @@ window.addEventListener("online", function(e) {
 	
 	   }else if(HELP == 8){
 	//	if(Prem.value=="n")   window.location.href = "#myGame";
-	 if(Prem.value=="n")getReklama();
+	// if(Prem.value=="n")getReklama();
 	 HELP = 0;
 		//window.location.href = "#ozeniteHREF";
 	   }else if(HELP == 5){
