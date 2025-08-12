45359f844e6b4435e79a2fb161fba259ef424d51
Globik <globalikslivov@gmail.com>
2025-08-12 19:08:27 +0500

    a

--- views/main.js
+++ views/main.js
@@ -725,7 +725,7 @@ if(gid("giftbox2"))gid('giftbox2').style.display='none';
 			"platform":"touch",
 			"onClose":function(){
 			console.log("Reklama closed")
-			setTimeout(function(){getReklama();}, 1000 * 30)
+			setTimeout(function(){getReklama();}, 1000 * 10)
 			if(kkk == 1){
 			//if(isLogin.value=="true")
 		//	window.location.href="#setPrem";
